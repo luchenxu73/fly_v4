e0a5457fe070effa3875d59405336af4f8ea9940
luchenxu73 <lcxemail73@gmail.com>
2022-08-10 10:44:10 +0800

    Initial commit

---
+++ User/inc/hello.h
@@ -0,0 +1,9 @@
+//
+// Created by Luchenxu on 2022/8/10.
+//
+
+#ifndef FLY_V4_HELLO_H
+#define FLY_V4_HELLO_H
+int Hello();
+#include "stm32f1xx_hal.h"
+#endif //FLY_V4_HELLO_H
---
+++ User/src/hello.c
@@ -0,0 +1,8 @@
+//
+// Created by Luchenxu on 2022/8/10.
+//
+#include "hello.h"
+int Hello()
+{
+   return 0;
+}
